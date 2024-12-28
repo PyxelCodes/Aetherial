64709b40ffdd89d919d24aec47b478f31cf70c7e
PyxelCodes <pyxeldev@icloud.com>
2024-12-28 20:27:03 +0100

    remove logs

--- src/debug/logger.ts
+++ src/debug/logger.ts
@@ -1,9 +1,10 @@
-import signale from 'signale';
+//import signale from 'signale';
 
 export function log(...args: string[]) {
-    if (process.env.NODE_ENV === 'development') {
-        console.log("\n//#region DEBUG LOG \n\n");
-        signale.debug(...args);
-        console.log("\n//#endregion\n");
+    args.forEach(() => {});
+    if (process.env.NODE_ENV === "development") {
+        // console.log("\n//#region DEBUG LOG \n\n");
+        // signale.debug(...args);
+        // console.log("\n//#endregion\n");
     }
 }
